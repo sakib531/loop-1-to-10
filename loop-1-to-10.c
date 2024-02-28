cbe4a8d7c4299878d736f1343f1d4a06d126ed2f
sakib531 <142710106+sakib531@users.noreply.github.com>
2024-02-28 23:38:54 +0600

    Add files via upload

---
+++ loop-1-to-10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (){
+    int x,n;
+    printf("input the ending number : ");
+    scanf("%d",&n);
+        for (x=1;x<=n;x++){
+            printf("%d\n",x);
+        }
+    return 0;
+}
